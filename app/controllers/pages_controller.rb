fb3320e61b8f0834124c0acd9d9a0880a1244d93
hsanka6 <haasith2@gmail.com>
2016-04-24 00:43:36 +0000

    Updated branch with project files

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  #each function definition refers to the html view in the pages folder
+  def home 
+    
+  end
+  
+  def about
+    
+  end
+  
+  def login
+    
+  end
+  def signUp
+  end
+  
+end
