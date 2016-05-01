6279184a3dc4157a43b5f39935f4404e694cf5a8
hsanka6 <haasith2@gmail.com>
2016-05-01 04:39:59 +0000

    merging and profile edit

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get 'edit', to: 'pages#edit'
   get 'setting', to: 'pages#setting'
   resources :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
