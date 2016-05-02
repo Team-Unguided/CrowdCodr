e001a2055cfcdfa334a1280b76970f2a3eb06ca5
andythio <andythio@live.com>
2016-05-02 18:40:03 +0000

    Hotfix: Broken Show

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,7 +1,11 @@
 module ListingsHelper
   #Returns true if current user is owner of listing
   def is_owner?(listing)
-    listing.user_id == current_user.id
+    if logged_in?
+      listing.user_id == current_user.id
+    else
+      false
+    end
   end
   
   # Returns true if the currnet profile owns the listing
