86e1703311359cbe8ed66920ed09c0a30ca70f0c
Kenneth Huang <khuan013@ucr.edu>
2016-05-05 05:48:15 +0000

    Added a simple direct messaging function

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505020744) do
+ActiveRecord::Schema.define(version: 20160505045558) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id",    limit: 4
+    t.integer  "recipient_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "listings", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -22,6 +29,18 @@ ActiveRecord::Schema.define(version: 20160505020744) do
     t.datetime "updated_at",                null: false
     t.integer  "user_id",     limit: 4
   end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body",            limit: 65535
+    t.integer  "conversation_id", limit: 4
+    t.integer  "user_id",         limit: 4
+    t.boolean  "read",                          default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",      limit: 255
