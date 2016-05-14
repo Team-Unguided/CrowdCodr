8f2a26ec6292f722ce2592cbcb9ea5ef2f5dfe20
Shawn Lee <slee208@ucr.edu>
2016-05-14 02:35:25 +0000

    Removed some residue merge text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513000338) do
+ActiveRecord::Schema.define(version: 20160513215138) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id",    limit: 4
@@ -28,8 +28,6 @@ ActiveRecord::Schema.define(version: 20160513000338) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.integer  "user_id",     limit: 4
-    t.string   "projects",    limit: 255
-    t.float    "price",       limit: 53
   end
 
   create_table "messages", force: :cascade do |t|
@@ -43,17 +41,6 @@ ActiveRecord::Schema.define(version: 20160513000338) do
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
   add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
-
-  create_table "orders", force: :cascade do |t|
-    t.string   "address",    limit: 255
-    t.string   "city",       limit: 255
-    t.string   "state",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "listing_id", limit: 4
-    t.integer  "buyer_id",   limit: 4
-    t.integer  "seller_id",  limit: 4
-  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating",     limit: 4
