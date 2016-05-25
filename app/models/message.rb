f772d6f614b290afdd57c9da830bebfb28a5796d
Shawn Lee <slee208@ucr.edu>
2016-05-25 17:14:42 +0000

    Added ZIP to user search

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ActiveRecord::Base
     belongs_to :conversation
     belongs_to :user
     validates_presence_of :body, :conversation_id, :user_id
+    validates :body, presence: true, allow_nil: false
  
     def message_time
         created_at.strftime("%m/%d/%y at %l:%M %p")
