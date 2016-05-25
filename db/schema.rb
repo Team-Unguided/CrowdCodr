c21d2727c03379712ed6d712f400b1eea69ccc6c
Harrison Ray <hray001@ucr.edu>
2016-05-25 12:05:51 +0000

    Clear for dev merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160513215138) do
+ActiveRecord::Schema.define(version: 20160518044504) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id",    limit: 4
@@ -33,17 +32,6 @@ ActiveRecord::Schema.define(version: 20160513215138) do
     t.float    "price",       limit: 53
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.string   "address",    limit: 255
-    t.string   "city",       limit: 255
-    t.string   "state",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "listing_id", limit: 4
-    t.integer  "buyer_id",   limit: 4
-    t.integer  "seller_id",  limit: 4
-  end
-
   create_table "messages", force: :cascade do |t|
     t.text     "body",            limit: 65535
     t.integer  "conversation_id", limit: 4
@@ -55,6 +43,17 @@ ActiveRecord::Schema.define(version: 20160513215138) do
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
   add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "address",    limit: 255
+    t.string   "city",       limit: 255
+    t.string   "state",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "listing_id", limit: 4
+    t.integer  "buyer_id",   limit: 4
+    t.integer  "seller_id",  limit: 4
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating",     limit: 4
@@ -73,13 +72,10 @@ ActiveRecord::Schema.define(version: 20160513215138) do
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
     t.string   "password_digest", limit: 255
-<<<<<<< HEAD
     t.string   "recipient",       limit: 255
-=======
     t.text     "description",     limit: 65535
     t.string   "picture",         limit: 255
     t.string   "zipcode",         limit: 255
->>>>>>> origin/develop
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
