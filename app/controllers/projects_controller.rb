5813c32f1c5113f6ff7f8d4b65b5ad30a9dc4d1c
Shawn Lee <slee208@ucr.edu>
2016-06-01 01:00:39 +0000

    Updated profile, fixed project path

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -42,7 +42,7 @@ class ProjectsController < ApplicationController
   def update
     respond_to do |format|
       if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.html { redirect_to user_path(current_user), notice: 'Project was successfully updated.' }
         format.json { render :show, status: :ok, location: @project }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class ProjectsController < ApplicationController
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.html { redirect_to user_path(current_user), notice: 'Project was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531231441) do
+ActiveRecord::Schema.define(version: 20160531233511) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id",    limit: 4
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20160531231441) do
     t.string   "languages",   limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "user_id",     limit: 4
   end
 
   create_table "relationships", force: :cascade do |t|
